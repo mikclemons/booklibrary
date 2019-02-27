3271c2217ed20b27cfbfb0c7fd114cf51fed9e7e
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-02-27 11:32:48 -0500

    Create Book class with accessor methods and multiple constructors.

---
+++ src/main/java/com/detroitlabs/booklibrary/Model/Book.java
@@ -0,0 +1,68 @@
+package com.detroitlabs.booklibrary.Model;
+
+import java.time.LocalDate;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private LocalDate checkOutDate;
+    private int genre;
+    private boolean isCheckedOut;
+
+    public Book(String title, String author, LocalDate checkOutDate, int genre, boolean isCheckedOut) {
+        this.title = title;
+        this.author = author;
+        this.checkOutDate = checkOutDate;
+        this.genre = genre;
+        this.isCheckedOut = isCheckedOut;
+    }
+
+    public Book(String title, String author, int genre, boolean isCheckedOut) {
+        this.title = title;
+        this.author = author;
+        this.checkOutDate = LocalDate.now();
+        this.genre = genre;
+        this.isCheckedOut = isCheckedOut;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public int getGenre() {
+        return genre;
+    }
+
+    public void setGenre(int genre) {
+        this.genre = genre;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+}
